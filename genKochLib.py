91329b14bbbc5a6a02e9310dbc9ba70675cad555
davidenole <davide.nole93@gmail.com>
2017-02-11 18:15:01 +0100

    Update 1
    Now it works, though I am a bit sad that only up to n_sides = 4 it works decently (theoretically speaking) and then nope...

--- genKochLib.py
+++ genKochLib.py
@@ -1,5 +1,3 @@
-''' Crappy version 0.0 '''
-
 import turtle as t
 
 class Snowflake:
@@ -20,7 +18,7 @@ class Snowflake:
         self.nSides = ns
         self.nIterations = nit
         self.side = l*1.0
-        self.angle = (ns-2)*360.0/ns
+        self.angle = (ns-2)*180.0/ns
         self.drawer.ht()
         self.drawer.pu()
         self.drawer.goto(X[0],X[1])
@@ -43,25 +41,30 @@ class Snowflake:
         if ni==0:
             self.drawer.fd(l)
         else:
-            self.drawer.fd(l/self.nSides)
+            self.dd(ni-1,l/self.nSides)
             for i in range(self.nSides-2):
                 if i%2==0: # out bump
                     self.drawer.rt(self.angle)
                     for k in range(self.nSides-1):
                         self.dd(ni-1,l/self.nSides)
+                        self.drawer.lt(180-self.angle)
                     self.drawer.rt(180)
                 else: # in bump
                     self.drawer.lt(self.angle)
                     for k in range(self.nSides-1):
                         self.dd(ni-1,l/self.nSides,False)
-                    self.drawer.rt(180)            
-        if control:
-            self.drawer.lt(self.angle)       
-        else:
-            self.drawer.rt(self.angle)
+                        self.drawer.rt(180-self.angle)
+                    self.drawer.rt(180)       
+            self.dd(ni-1,l/self.nSides)    
+
     # draws the snowflake
     def draw(self):
         for j in range(self.nSides):
             self.dd(self.nIterations,self.side)
+            self.drawer.lt(180-self.angle)
+        
+        
+        
+        
         
         
